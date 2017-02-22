8fdd15417fd4811f9c8b3178ff1b0396af29e671
Jason Barbier <jabarb@corrupted.io>
2017-02-22 06:30:07 -0800

    updates to scripts

--- Add-SCEPExclusionPath.ps1
+++ Add-SCEPExclusionPath.ps1
@@ -1,3 +1,17 @@
+<#
+Copyright 2017 Jason Barbier (jason@corrupted.io)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#>
+
 function Add-SCEPExclusionPath
 {
     <#
@@ -16,33 +30,42 @@ function Add-SCEPExclusionPath
     [cmdletbinding()]
     param
     (
-        [Parameter(HelpMessage='This is a string array of paths to add to the exclusion list')]
-        [ValidateNotNullOrEmpty()]
-        [string[]]$ExclusionPathToAdd = '',
-
-        [Parameter(HelpMessage='A string array of computers to run this on, defaults to the local machine')]
+        [Parameter(
+            HelpMessage='A string array of computers to run this on, defaults to the local machine',
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true,
+            Position=0)]
         [ValidateNotNullOrEmpty()]
         [string[]]$ComputerName = $env:COMPUTERNAME,
 
-        [Parameter(HelpMessage='This is a cimsessionoption to deal with cim sessions. the default is protocol=DCOM')]
+        [Parameter(
+            HelpMessage='This is a string array of paths to add to the exclusion list',
+            Position=1)]
+        [ValidateNotNullOrEmpty()]
+        [string[]]$ExclusionPathToAdd = '',
+
+        [Parameter(
+            HelpMessage='This is a cimsessionoption to deal with cim sessions. the default is protocol=DCOM',
+            Position=2)]
         [ValidateNotNullOrEmpty()]
         [CimSessionOptions]$CimSessionOptions = (New-CimSessionOption -Protocol Dcom)
     )
-    try {
-        $progress = 1
-        foreach ($Computer in $ComputerName) {
-            Write-Progress -Activity 'Setting Exclusion Path' -Status $Computer -PercentComplete ($progress/($ComputerName.count))
-            Write-Verbose -Message "Starting work on $Computer"
+    process 
+    {
+        try 
+        {
+            Write-Progress -Activity 'Setting Exclusion Path' -Status $ComputerName
+            Write-Verbose -Message "Starting work on $ComputerName"
 
             if ($CimSessionOptions) {
                 $SessionArguments = @{
                     'SessionOption' = $CimSessionOptions
-                    'ComputerName' = $Computer
+                    'ComputerName' = $ComputerName
                 }
             }
             else {
                 $SessionArguments = @{
-                    'ComputerName' = $Computer
+                    'ComputerName' = $ComputerName
                 }
             }
 
@@ -61,10 +84,10 @@ function Add-SCEPExclusionPath
                 Write-Error 'Could not find System Endpoint Protection'
             }
         }
-        $progress++
-    }
-    catch {
-        write-error $_
-        break
+        catch 
+        {
+            write-error $_
+            break
+        }
     }
 }
--- Start-UDPPacketGenetator.ps1
+++ Start-UDPPacketGenetator.ps1
@@ -12,44 +12,71 @@ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTH
 WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #>
 
-function Start-UDPPacketGenerator {
+function Start-UDPPacketGenerator 
+{
 	param
 	(
+		[Parameter(HelpMessage='Ip address you want to send UDP packets to')][ValidateNotNullOrEmpty()]
 		[System.Net.IPaddress]$TargetIP = 255.255.255.255,
-		[int]$TargetPort = 2501,
-		[TimeSpan]$Duration = (New-Timespan -Seconds 5),
-        [Int]$SleepDuration
-	)
-	# Generate around a max udp packets worth of random data
-	$set = 'azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN0123456789-_'
-	$SendBuffer = 1..4 | ForEach-Object {
-		$rndChars = $set | Get-Random -Count $set.Count
-		$str = -join $rndChars
-		# repeat random string 4096 times to get a 256KB string
-		$str * 4
-	}
-	# Make it bytes and store it for use
-	$send = [System.Text.Encoding]::ASCII.GetBytes($SendBuffer)
-	
-	# Set up the UDP send
-	$ping = new-object System.Net.Sockets.UdpClient
-	$ping.EnableBroadcast = $true
-	$ping.Connect($targetip,$targetport)
 
-	# Send for the duration in packets that was specified
-	$Timer = [Diagnostics.Stopwatch]::StartNew()
-	while ($Timer.Elapsed -lt $duration)
+		[Parameter(HelpMessage='Port to send the packets to')][ValidateNotNullOrEmpty()]
+		[int]$TargetPort = 2501,
+
+		[Parameter(HelpMessage='How long do we want to generate packets for?')][ValidateNotNullOrEmpty()]
+		[TimeSpan]$Duration = (New-Timespan -Seconds 5),
+
+		[Parameter(HelpMessage='if you want any sort of delay between packets to help with rate limiting (int in miliseconds)')]
+        [Int]$SleepDuration,
+
+		[Parameter(HelpMessage='Are we trying to send a message?')]
+		[string]$Message
+	)
+	begin
 	{
-		$null = $ping.Send($send,$send.count)
-        if ($SleepDuration) {
-            Start-Sleep -Milliseconds $SleepDuration
-        }
+		# Generage a byte payload, if there isnt a message specified make up some random garbage
+		if (!$Message)
+		{
+			# Generate around a max udp packets worth of random data
+			$set = 'azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN0123456789-_'
+			$SendBuffer = 1..4 | ForEach-Object {
+				$rndChars = $set | Get-Random -Count $set.Count
+				$str = -join $rndChars
+				# repeat random string 4096 times to get a 256KB string
+				# this is roughly the max size of a UDP packet without fragmenting.
+				$str * 4
+			}
+			# Make it bytes and store it for use
+			$Payload = [System.Text.Encoding]::UTF8.GetBytes($SendBuffer)
+		}
+		# Otherwise just byte encode the message
+		else 
+		{
+			$Payload = [System.Text.Encoding]::UTF8.GetBytes($Message)
+		}
+	}
+	process
+	{	
+		# Set up the UDP send
+		$ping = new-object System.Net.Sockets.UdpClient
+		$ping.EnableBroadcast = $true
+		$ping.Connect($targetip,$targetport)
+
+		# Send for the duration in packets that was specified
+		$Timer = [Diagnostics.Stopwatch]::StartNew()
+		while ($Timer.Elapsed -lt $duration)
+		{
+			# Send() will always print out the amout of bytes sent
+			# as such we just tell it to send that data to null,
+			# We are really only worried about sending not what we sent.
+			$null = $ping.Send($Payload,$Payload.count)
+			if ($SleepDuration) {
+				Start-Sleep -Milliseconds $SleepDuration
+			}
+		}
+		$Timer.Stop()
 	}
 	end
 	{
-		$Timer.stop()
 		$Ping.Close()
-		$Ping.Finalize()
-		$Ping.Dispose()
 	}
 }
